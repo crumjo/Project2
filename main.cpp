3cfc9c36a5ec2b020b20fab42afa67519c292a65
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2017-10-01 10:58:09 -0400

    added the pre-generated xcode main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basic
+//
+//  Created by Joshua Crum on 10/1/17.
+//  Copyright © 2017 Joshua Crum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
